cfb32905891d5007f4cc0f5314f7ee3f0b87433b
Justin Tingley <justin@bocbanking.com>
2023-09-19 18:28:23 -0500

    part 5

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
-package database
+package controllers
+
+import ()
+
+var (
+	ErrCantFindProduct    = error.New("can't find the product")
+	ErrCantDecodeProduct  = error.New("can't find the product.")
+	ErrUserIdIsNotValid   = error.New("this user is not valid")
+	ErrCantUpdateUser     = error.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = error.New("can't remove this item from the cart")
+	ErrCantGetItem        = error.New("unable to get the item from the cart")
+	ErrCantBuyCartItem    = error.New("can't update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstanBuyer() {
+
+}
