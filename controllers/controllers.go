f087db0fff74535ace9872f5ff926a5ad4659cb9
Justin Tingley <justin@bocbanking.com>
2023-09-19 19:44:27 -0500

    part 7

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -55,11 +55,69 @@ func Signup() gin.HandlerFunc {
 			return
 		}
 
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(user.Email, *user.First_Name, *user.Last_Name, *user, User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user was not created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, gin.H{"message": "user created successfully"})
+
 	}
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeOut(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return
+		}
+
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid email or password"})
+			return
+		}
+
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+
+		defer cancel()
+
+		if !PasswordIsValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println(msg)
+			return
+		}
+		token, refreshtoken, _ := generate.TokenGenerator(*founduser.Email, *founduser.First_Name, *founduser.Last_Name, *founduser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshtoken, *founduser.User_ID)
+
+		c.JSON(http.StatusFound, founduser)
+
+	}
 }
 
 func ProductViewerAdmin() gin.HandlerFunc {
