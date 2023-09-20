3844ef9ece750c5a0bce630ed35329d8095a1a00
Justin Tingley <justin@bocbanking.com>
2023-09-20 11:48:30 -0500

    part 18

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,1 +1,109 @@
 package tokens
+
+import (
+	"context"
+	"log"
+	"time"
+	"os"
+	"database"
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+type SignedDetails struct {
+	Email string 
+	First_Name string
+	Last_Name string
+	Uid string
+	jwt.StandardClaims
+}
+
+var UserData *mongo.Collection = database.userData(database.Client, "Users")
+var SECRET_KEY - os.Getenv("SECRET_KEY")
+
+func TokenGenerator(email string, firstname string, lastname string, uid string)(signedtoken string, signedrefreshtoken, string, err error) {
+
+	claims := &SignedDetails{
+		Email: email,
+		First_Name: firstname,
+		Last_Name: lastname,
+		Uid: uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshclaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshtoken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshclaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.panic(err)
+		return
+	}
+
+	return token, refreshtoken, err
+
+
+}
+
+func ValidateToken(signedtoken string ) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(signedtoken, &SignedDetails{}, func(token *jwt.Token)(interface{}, error){
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = "Invalid Token"
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = "Token Expired"
+		return
+	}
+	return claims, msg
+}
+	
+
+func UpdateAllToken(signedtoken string, signedrefreshtoken string, userid string) {
+
+	var ctx, canel = context.WithTimeout(context.Background(), 100*time.Second)
+	var updateobj primitivie.D
+
+	updateobj = append (updateobj, bson.E{Key: "token", Value: signedtoken})
+	updateobj = append (updateobj, bson.E{Key: "refresh_token", Value: signedrefreshtoken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateobj = append (updateobj, bson.E{Key: "updated_at", Value: updated_at})
+
+	upsert := true
+
+	filter := bson.M{"_id": userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := UserData.UpdateOne(ctx, filter, bson.D{
+		{"$set", updateobj}}, &opt)
+	defer cancel()
+
+	if err != nil {
+		log.panic(err)
+		return
+	}
+
+}
